78ec332229484c30b9da88e14a7d505566ac73d6
canba <canba@DESKTOP-KKDKFE0>
2025-02-12 15:04:42 +0300

    Add project files.

---
+++ love/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace love.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            DateTime startDate = new DateTime(2022, 09, 14, 00, 00, 00); // İlk gününüzü buraya gir
+            ViewBag.StartDate = startDate.ToString("yyyy-MM-ddTHH:mm:ss"); // JS için formatlıyoruz
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ love/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="love.MvcApplication" Language="C#" %>
